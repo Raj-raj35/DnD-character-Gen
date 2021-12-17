9596447575eb3cc012fb54c977f90b2fbb638ce6
Raj-raj35 <92857422+Raj-raj35@users.noreply.github.com>
2021-12-17 06:07:39 +0000

    222

--- scripts/apptest.sh
+++ scripts/apptest.sh
@@ -1,13 +1,13 @@
-sudo chmod 666 /var/run/docker
-sudo apt update
+# sudo chmod 666 /var/run/docker
+ sudo apt update
 sudo apt upgrade
 sudo apt install -y python3 python3-pip python-pytest
 sudo pip3 install --upgrade pip
 
 cd frontend
-#pip3 install -r requirements.txt
-#python3 test_front_end.py
-#pytest --cov=application
+# pip3 install -r requirements.txt
+# python3 test_front_end.py
+# pytest --cov=application
 python3 -m pytest --cov=application
 
 cd ../backend
