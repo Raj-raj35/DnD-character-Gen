3a0f6c8cc3cada6bbebe95a698216720b525ed64
raj-raj35 <raj_raj35@hotmail.com>
2021-12-17 05:51:50 +0000

    tests

--- scripts/apptest.sh
+++ scripts/apptest.sh
@@ -5,12 +5,13 @@ sudo apt install -y python3 python3-pip
 sudo pip3 install --upgrade pip
 
 cd frontend
-pip3 install -r requirements.txt
-python3 test_front_end.py
-pytest --cov=application
+#pip3 install -r requirements.txt
+#python3 test_front_end.py
+#pytest --cov=application
+python3 -m pytest --cov=application
 
 cd ../backend
-python3 pytest --cov=application
+python3 -m pytest --cov=application
 
 cd ../service1
 python3 -m pytest --cov=application
